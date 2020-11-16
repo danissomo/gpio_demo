b37b1edb778393a085023ac42b1747ffdeadcd40
danissomo <dan.2k64@gmai.com>
2020-11-17 00:35:37 +0300

    set all high, unexport and read

--- main.c
+++ main.c
@@ -7,4 +7,13 @@ int main(){
 		set_direction(pinouts[i], OUTPUT);
 		set_pin(pinouts[i], 1);
 	}
+	getc(stdin);
+	for(int i= 0; i < 40; i++)
+		unexport_pin(pinouts[i]);
+	getc(stdin);
+	for(int i= 0; i < 40; i++)
+		export_pin(pinouts[i]);
+	for(int i= 0; i < 40; i++)
+		printf("%d : %d\n",i , read_pin(pinouts[i]));
+	
 }
